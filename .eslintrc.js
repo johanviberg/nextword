f096d1239c1e44d9c0b9ec4dc8b30da6d2a955df
Johan Viberg <johan@remixventures.com>
2024-02-20 14:48:35 +0100

    fix: change max length in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,17 @@ module.exports = {
   rules: {
     'no-unused-vars': 'off',
     'no-console': 'warn',
+    'max-len': [
+      'error',
+      {
+        code: 140,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreUrls: true,
+      },
+    ],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react/no-unescaped-entities': 'off',
-
     'react/display-name': 'off',
     'react/jsx-curly-brace-presence': [
       'warn',
