152d50cbf1095a9e5f1533b88bb16ba3812c422d
Johan Viberg <johan@remixventures.com>
2024-02-28 15:56:10 +0100

    fix: remove unused SVG animation on start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,9 @@ import * as React from "react";
 
 import { Button } from "@/components/ui/button";
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Logo from "~/svg/BlocksShuffle.svg";
-
 export default function HomePage() {
   return (
     <div>
-      <Logo className="w-64" />
       <h1 className="mt-4">nextword</h1>
       <p className="mt-2 text-sm text-gray-800">
         Here is a shadcn button. <Button>Hello</Button>
