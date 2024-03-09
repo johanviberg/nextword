627d66135be2b84bcfacd7057ad12d199fbf676b
Johan Viberg <johan@remixventures.com>
2024-03-09 13:45:12 +0100

    fix: improve site title for 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { RiAlarmWarningFill } from "react-icons/ri";
 
 export const metadata: Metadata = {
-  title: "Not Found",
+  title: "Nextword | Page Not Found",
 };
 
 export default function NotFound() {
