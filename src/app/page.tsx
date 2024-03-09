b01b691cd01f31f4359d0725873f11332f9f3af1
Johan Viberg <johan@remixventures.com>
2024-03-09 18:31:44 +0100

    refactor: update placeholder text on start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,32 @@
 
 import * as React from "react";
 
-import { Button } from "@/components/ui/button";
-
 export default function HomePage() {
   return (
     <div>
-      <h1 className="mt-4">nextword</h1>
+      <h1 className="mt-4 text-xl">Get Started</h1>
       <p className="mt-2 text-sm text-gray-800">
-        Here is a shadcn button. <Button>Hello</Button>
+        If onboarding is not complete, show onboarding component, and if yes, show the dashboard.
       </p>
+      <h1 className="mt-4 text-xl">Onboarding (Hide)</h1>
+      <ul className="mt-5">
+        <li>Invite your team (assuming first, admin user) [CTA button]</li>
+        <li>Create an article [CTA button]</li>
+        <li>Connect a CMS [CTA button]</li>
+        <li>Publish an article to a CMS [CTA button]</li>
+      </ul>
+      <h1 className="mt-4 text-xl">Dashboard</h1>
+      <p>Show empty state like Vercel Analytics</p>
+      <p>Date Range: Last 24 Hours, Last 7 Days, Last 30 Days, Last 3 Months, Last 12 Months </p>
+      <ul className="mt-5">
+        <li>Articles created (single value)</li>
+        <li>Articles published (single value)</li>
+        <li>Articles created over time (graph)</li>
+        <li>Articles published over time (graph)</li>
+        <li>Average word count per article</li>
+        <li>Credits Remaining</li>
+        <li>Credits Used</li>
+      </ul>
     </div>
   );
 }
