9aad32da5f40b81043003185db9c9f9bded3e5b5
Johan Viberg <johan@remixventures.com>
2024-03-11 22:13:06 +0100

    feat: update inngest function to next generation

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,7 +2,7 @@ import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction({ id: "hello-world" }, { event: "test/hello.world" }, async ({ event, step }) => {
   await step.sleep("wait-a-moment", "20s");
-  return { event, body: "Hello, World!" };
+  return { event, body: "Hello, World V2!" };
 });
 
 // TODO: Add functions to handle article generation (write pseudo code first based on steps and error handling)
